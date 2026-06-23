d72127e18a41adf645cf94a3c0024dae62de8a8c
kymin <park48664866@gmail.com>
2026-06-24 00:15:41 +0900

    feat: 응답 코드 인터페이스 구현

---
+++ routee-common/src/main/java/org/sopt/routee/code/ErrorResultCode.java
@@ -0,0 +1,7 @@
+package org.sopt.routee.code;
+
+/**
+ * 실패 응답 마커 인터페이스
+ */
+public interface ErrorResultCode extends ResultCode {
+}
---
+++ routee-common/src/main/java/org/sopt/routee/code/SuccessResultCode.java
@@ -0,0 +1,7 @@
+package org.sopt.routee.code;
+
+/**
+ * 성공 응답 마커 인터페이스
+ */
+public interface SuccessResultCode extends ResultCode {
+}
