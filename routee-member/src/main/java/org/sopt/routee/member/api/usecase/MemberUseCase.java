89e259ac8d83d3bdb0853188c10c682fd4fad615
kymin <park48664866@gmail.com>
2026-07-06 15:37:03 +0900

    feat: member 모듈의 공개 영역 설정

---
+++ routee-member/src/main/java/org/sopt/routee/member/api/usecase/MemberUseCase.java
@@ -0,0 +1,10 @@
+package org.sopt.routee.member.api.usecase;
+
+import org.sopt.routee.external.api.type.OAuthProvider;
+import org.sopt.routee.member.api.result.TokenClaimsResult;
+
+public interface MemberUseCase {
+
+	TokenClaimsResult getTokenResult(String oauthId, OAuthProvider oauthProvider);
+
+}
