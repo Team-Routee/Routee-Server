a818d1715bf1602a3a4feb1f0f9d7dbac5e96a8c
youtheyeon <yuseoyeon@ewhain.net>
2026-07-07 02:28:04 +0900

    feat: Route 조회/삭제 레포지토리 메서드 추가

--- routee-activity/src/main/java/org/sopt/routee/activity/internal/repository/RouteRepository.java
+++ routee-activity/src/main/java/org/sopt/routee/activity/internal/repository/RouteRepository.java
@@ -1,7 +1,13 @@
 package org.sopt.routee.activity.internal.repository;
 
+import java.util.List;
+
 import org.sopt.routee.activity.internal.entity.route.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+	List<Route> findByActivityIdOrderBySequenceAsc(Long activityId);
+
+	void deleteByActivityId(Long activityId);
 }
