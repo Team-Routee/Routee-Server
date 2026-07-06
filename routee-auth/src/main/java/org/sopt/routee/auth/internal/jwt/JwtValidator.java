b10c68765e80825e2b204117b2587b240bdb975e
kymin <park48664866@gmail.com>
2026-07-06 11:06:13 +0900

    feat: issuer 검증 추가

--- routee-auth/src/main/java/org/sopt/routee/auth/internal/jwt/JwtValidator.java
+++ routee-auth/src/main/java/org/sopt/routee/auth/internal/jwt/JwtValidator.java
@@ -2,6 +2,7 @@ package org.sopt.routee.auth.internal.jwt;
 
 import javax.crypto.SecretKey;
 
+import org.sopt.routee.auth.internal.config.JwtProperties;
 import org.sopt.routee.auth.internal.exception.InvalidTokenException;
 import org.sopt.routee.auth.internal.exception.TokenExpiredException;
 import org.springframework.stereotype.Component;
@@ -17,11 +18,13 @@ import lombok.RequiredArgsConstructor;
 public class JwtValidator {
 
 	private final SecretKey jwtSecretKey;
+	private final JwtProperties jwtProperties;
 
 	public Claims validate(String token) {
 		try {
 			return Jwts.parser()
 				.verifyWith(jwtSecretKey)
+				.requireIssuer(jwtProperties.issuer())
 				.build()
 				.parseSignedClaims(token)
 				.getPayload();
