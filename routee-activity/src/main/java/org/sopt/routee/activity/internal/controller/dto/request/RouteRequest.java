bbe913a630501f5b416254bd9676e7d551e4f06d
youtheyeon <yuseoyeon@ewhain.net>
2026-07-07 15:00:29 +0900

    refactor: route sequence 검증을 1 이상으로 변경

--- routee-activity/src/main/java/org/sopt/routee/activity/internal/controller/dto/request/RouteRequest.java
+++ routee-activity/src/main/java/org/sopt/routee/activity/internal/controller/dto/request/RouteRequest.java
@@ -4,14 +4,14 @@ import org.sopt.routee.activity.internal.service.dto.command.CreateRouteCommand;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
 
 public record RouteRequest(
 	@NotBlank(message = "name은 필수입니다.")
 	String name,
 
 	@NotNull(message = "sequence는 필수입니다.")
-	@PositiveOrZero(message = "sequence는 0 이상이어야 합니다.")
+	@Positive(message = "sequence는 1 이상이어야 합니다.")
 	Integer sequence
 ) {
 	public CreateRouteCommand toCommand() {
