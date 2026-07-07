03a31b52556aa74329427513cef1d0112507163a
khj011219 <khj011219@naver.com>
2026-07-08 00:04:40 +0900

    feat: timezone 변환 유틸 메서드 추가

--- routee-common/src/main/java/org/sopt/routee/util/TimeZoneUtils.java
+++ routee-common/src/main/java/org/sopt/routee/util/TimeZoneUtils.java
@@ -2,14 +2,28 @@ package org.sopt.routee.util;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public final class TimeZoneUtils {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-	private TimeZoneUtils() {
-	}
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TimeZoneUtils {
 
 	public static LocalDate toLocalDate(Instant instant, ZoneId zoneId) {
 		return instant.atZone(zoneId).toLocalDate();
+	}
+
+	public static LocalDateTime toLocalDateTime(Instant instant, ZoneId zoneId) {
+		return instant.atZone(zoneId).toLocalDateTime();
+	}
+
+	public static Instant toUtcInstant(LocalDate localDate, ZoneId zoneId) {
+		return localDate.atStartOfDay(zoneId).toInstant();
+	}
+
+	public static Instant toUtcInstant(LocalDateTime localDateTime, ZoneId zoneId) {
+		return localDateTime.atZone(zoneId).toInstant();
 	}
 }
