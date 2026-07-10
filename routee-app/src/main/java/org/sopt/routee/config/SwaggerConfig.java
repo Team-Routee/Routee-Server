afb83df18f8bf21243487b8383123b1385185a01
youtheyeon <yuseoyeon@ewhain.net>
2026-07-10 16:30:55 +0900

    chore: Swagger 타이틀 설정

--- routee-app/src/main/java/org/sopt/routee/config/SwaggerConfig.java
+++ routee-app/src/main/java/org/sopt/routee/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
@@ -15,6 +16,10 @@ class SwaggerConfig {
 	@Bean
 	OpenAPI openAPI() {
 		return new OpenAPI()
+			.info(new Info()
+					.title("Routee API")
+					.description("SOPT 38기 APPJAM Routee API 문서입니다.")
+					.version("v1.0.0"))
 			.components(new Components()
 				.addSecuritySchemes(SECURITY_SCHEME_NAME,
 					new SecurityScheme()
