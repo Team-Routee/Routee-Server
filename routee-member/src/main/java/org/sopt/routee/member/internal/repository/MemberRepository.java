e656e9a26d623ac009ad960f25d4b6bb47fc1345
kymin <park48664866@gmail.com>
2026-07-13 22:31:03 +0900

    feat: 총 활동 횟수 누적 기능 구현

--- routee-member/src/main/java/org/sopt/routee/member/internal/repository/MemberRepository.java
+++ routee-member/src/main/java/org/sopt/routee/member/internal/repository/MemberRepository.java
@@ -5,11 +5,18 @@ import java.util.Optional;
 import org.sopt.routee.external.api.type.OAuthProvider;
 import org.sopt.routee.member.internal.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByOauthIdAndOauthProvider(String oauthId, OAuthProvider oauthProvider);
 
 	boolean existsByOauthIdAndOauthProvider(String oauthId, OAuthProvider oauthProvider);
+
+	@Modifying
+	@Query("UPDATE Member m SET m.totalActivityCount = m.totalActivityCount + 1 WHERE m.id = :memberId")
+	void incrementTotalActivityCount(@Param("memberId") Long memberId);
 
 }
