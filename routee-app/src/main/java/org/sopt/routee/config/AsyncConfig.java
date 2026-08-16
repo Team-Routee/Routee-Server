a3a66b04ccc9c4018938d98c403d5e98e008b101
youtheyeon <yuseoyeon@ewhain.net>
2026-08-17 04:00:23 +0900

    fix: 타임라인 삭제 이벤트 리스너 @EnableAsync 누락으로 동기 실행되던 문제 수정

---
+++ routee-app/src/main/java/org/sopt/routee/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package org.sopt.routee.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+}
