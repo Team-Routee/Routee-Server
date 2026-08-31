25852180ba93c1cac19dee367099de134d80e0a4
kymin <park48664866@gmail.com>
2026-08-31 13:49:09 +0900

    fix: 누락된 필드 추가

--- routee-member/src/main/java/org/sopt/routee/member/internal/controller/dto/request/RegisterRequest.java
+++ routee-member/src/main/java/org/sopt/routee/member/internal/controller/dto/request/RegisterRequest.java
@@ -17,9 +17,12 @@ public record RegisterRequest(
 	String idToken,
 	@NotBlank
 	@Pattern(regexp = "^(?=.{1,12}$)[가-힣a-zA-Z0-9]+(?: [가-힣a-zA-Z0-9]+)*$", message = "닉네임은 한글, 영어, 숫자와 공백을 사용하여 1자 이상 12자 이하로 입력해야 하며, 공백은 연속될 수 없습니다.")
-	String nickname
+	String nickname,
+	@NotNull
+	@Valid
+	AgreementRequest agreements
 ) {
-	public RegisterCommand toCommand(ZoneId timeZone){
+	public RegisterCommand toCommand(ZoneId timeZone) {
 		return new RegisterCommand(provider, idToken, nickname, agreements.toCommand(), timeZone);
 	}
 }
