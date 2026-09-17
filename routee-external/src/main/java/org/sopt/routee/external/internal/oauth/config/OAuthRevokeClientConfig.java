17a2ce62b10dc8ff175e1b09dc2604e213c29c65
kymin <park48664866@gmail.com>
2026-09-18 01:47:58 +0900

    refactor: 외부 요청 timeout 설정

--- routee-external/src/main/java/org/sopt/routee/external/internal/oauth/config/OAuthRevokeClientConfig.java
+++ routee-external/src/main/java/org/sopt/routee/external/internal/oauth/config/OAuthRevokeClientConfig.java
@@ -1,14 +1,30 @@
 package org.sopt.routee.external.internal.oauth.config;
 
+import java.net.http.HttpClient;
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 class OAuthRevokeClientConfig {
 
+	private static final Duration CONNECT_TIMEOUT = Duration.ofSeconds(5);
+	private static final Duration READ_TIMEOUT = Duration.ofSeconds(10);
+
 	@Bean
 	RestClient oauthRevokeRestClient() {
-		return RestClient.create();
+		HttpClient httpClient = HttpClient.newBuilder()
+			.connectTimeout(CONNECT_TIMEOUT)
+			.build();
+
+		JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+		requestFactory.setReadTimeout(READ_TIMEOUT);
+
+		return RestClient.builder()
+			.requestFactory(requestFactory)
+			.build();
 	}
 }
